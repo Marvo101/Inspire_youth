2aaa92e9545fdcf9288898a24f8e4b0056a53704
Marvo101 <kinyanjuip249@gmail.com>
2023-02-20 11:42:11 +0300

    week 3

--- Week-3/Assignment-1.py
+++ Week-3/Assignment-1.py
@@ -4,3 +4,25 @@
 #sort the list
 #pop out two celebrities from the list
 #count the remaining celebs
+
+favourite_musicians=["Pop Smoke","Juice World"," Ben githae","Shiro wa Gp","Kenny Rogers","Flourida georgia"]
+print(favourite_musicians)
+print("-----------------")
+updated_celebrities=favourite_musicians + ("Size 8","Burna boy","Charlie Puth","Paul Mwai","Moji short Baba")
+updated_celebrities=updated_celebrities.append()
+print("updated_celebrities")
+
+updated_celebrities.copy()
+print(updated_celebrities)
+
+updated_celebrities.sort()
+print(updated_celebrities)
+
+updated_celebrities.pop()
+print(updated_celebrities)
+
+updated_celebrities.pop()
+print(updated_celebrities)
+
+updated_celebrities.count()
+print(updated_celebrities)
