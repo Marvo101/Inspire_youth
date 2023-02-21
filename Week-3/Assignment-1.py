2b1032c69e475dc9fc0fe82cb303410843739104
Marvo101 <kinyanjuip249@gmail.com>
2023-02-21 09:06:30 +0300

    week 3

--- Week-3/Assignment-1.py
+++ Week-3/Assignment-1.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+#this is a single line comment
+#python programme to illusrate operators in python
+#name :Marvin Kinyanjui
+#email :kinyanjuip249@gmail.com
+#Date :17th Feb2023
+#file :operators.py
+#!/usr/bin/en/python3
 #create an empty list of favourite musicians
 #Using for loop add atleast 5 new musicians
 #Copy the least of favourite musicians
@@ -5,24 +13,41 @@
 #pop out two celebrities from the list
 #count the remaining celebs
 
-favourite_musicians=["Pop Smoke","Juice World"," Ben githae","Shiro wa Gp","Kenny Rogers","Flourida georgia"]
-print(favourite_musicians)
+favourite_musicians1 = ["Pop Smoke","Juice World"," Ben githae","Shiro wa Gp","Kenny Rogers","Flourida georgia"]
+print(favourite_musicians1)
 print("-----------------")
-updated_celebrities=favourite_musicians + ("Size 8","Burna boy","Charlie Puth","Paul Mwai","Moji short Baba")
-updated_celebrities=updated_celebrities.append()
-print("updated_celebrities")
 
-updated_celebrities.copy()
-print(updated_celebrities)
+favourite_musicians2=["size 8","short baba","bahati","mabel","g-eazy"]
+print(favourite_musicians2)
 
-updated_celebrities.sort()
-print(updated_celebrities)
+for musicians in favourite_musicians2:
+    favourite_musicians1.append(musicians)
+print(favourite_musicians1)
 
-updated_celebrities.pop()
-print(updated_celebrities)
+favourite_musicians1.copy()
+print(favourite_musicians1)
 
-updated_celebrities.pop()
-print(updated_celebrities)
+favourite_musicians1.sort()
+print(favourite_musicians1)
 
-updated_celebrities.count()
-print(updated_celebrities)
+favourite_musicians1.pop()
+print(favourite_musicians1)
+
+favourite_musicians1.pop()
+print(favourite_musicians1)
+
+favourite_musicians1.count(6)
+print(favourite_musicians1)
+
+
+
+
+
+
+
+
+
+
+
+
+
